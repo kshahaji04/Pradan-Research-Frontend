770d04b7b2487369cb7214a45ba12651632e916a
shadab khan <shadab@8848digital.com>
2024-07-01 17:19:31 +0530

    core:initial commit:sk

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+    <h4>Research comming soon</h4>
+    </>
+  );
+}
