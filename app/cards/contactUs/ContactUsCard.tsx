4c9d12c58dc2f8f26d41000560a7df152f6e4372
amir rai <amir@8848Digital.com>
2024-07-23 16:26:51 +0530

    email and name link addes

--- app/cards/contactUs/ContactUsCard.tsx
+++ app/cards/contactUs/ContactUsCard.tsx
@@ -8,7 +8,7 @@ import { imageLoader } from '@/app/utils/image_loader_utils';
 
 
 
-const ContactUsCard = ({ e, idx, loading }:any) => {
+const ContactUsCard = ({ e, idx, loading }: any) => {
 
     return (
         <>
@@ -41,7 +41,7 @@ const ContactUsCard = ({ e, idx, loading }:any) => {
                                 }
                             </p>
                             <div className={`card-title ${styles.cardTitle}`}>
-                                <a href={`mailto:${e?.mail}`} className={styles.cardLink}>
+                                <a href={`mailto:${e?.email}`} className={styles.cardLink}>
                                     <span className='me-2'>
                                         <EmailIcon sx={{ fontSize: '1rem' }} />
                                     </span>
