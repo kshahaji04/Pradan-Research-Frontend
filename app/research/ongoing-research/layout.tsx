e8fe31396dc02faba21ac5d53deef91667ce0082
8848Digital-Rohit-Frontend <rohit.g@8848digital.com>
2024-07-25 17:07:44 +0530

    fix: research details page url fixed fix: rg

--- app/research/ongoing-research/layout.tsx
+++ app/research/ongoing-research/layout.tsx
@@ -3,12 +3,12 @@ import ResearchLayoutSkeleton from '@/app/skeletons/Research/ResearchLayoutSkele
 import React from 'react'
 
 function layout({ children }: any) {
-  const navItems = [
-    { label: 'About the Research', href: '/research/ongoing-research/about-the-research', type: 'link' },
-    { label: 'Publications', href: '/research/ongoing-research/publications', type: 'link' },
-    { label: 'Partners', href: '/research/ongoing-research/partners', type: 'link' },
-    { label: 'Queries', href: '/research', type: 'link' },
-  ];
+  // const navItems = [
+  //   { label: 'About the Research', href: '/research/ongoing-research/about-the-research', type: 'link' },
+  //   { label: 'Publications', href: '/research/ongoing-research/publications', type: 'link' },
+  //   { label: 'Partners', href: '/research/ongoing-research/partners', type: 'link' },
+  //   { label: 'Queries', href: '/research', type: 'link' },
+  // ];
 
   return (
     <>
